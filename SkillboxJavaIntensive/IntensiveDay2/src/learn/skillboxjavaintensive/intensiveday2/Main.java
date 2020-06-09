92915fea3b7ab5fd919da03413e851bb4ad51e80
Leonooore <eelyannora13@gmail.com>
2020-06-10 02:30:33 +0300

    skillbox intensive day 2 first commit

--- SkillboxJavaIntensive/IntensiveDay2/src/learn/skillboxjavaintensive/intensiveday2/Main.java
+++ SkillboxJavaIntensive/IntensiveDay2/src/learn/skillboxjavaintensive/intensiveday2/Main.java
@@ -1,8 +1,16 @@
 package learn.skillboxjavaintensive.intensiveday2;
 
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String ACCESS_TOKEN = "8GpIngND9yAAAAAAAAAAN4UuZ2e0aqWAOPwb-N5pMGY__cqCK-Hz9Ny6-tm9rvXo";
 
+    public static void main(String[] args) {
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+        MyThread thread = new MyThread();
+        thread.client = new DbxClientV2(config, ACCESS_TOKEN);
+        thread.start();
     }
 }
--- SkillboxJavaIntensive/IntensiveDay2/src/learn/skillboxjavaintensive/intensiveday2/MyThread.java
+++ SkillboxJavaIntensive/IntensiveDay2/src/learn/skillboxjavaintensive/intensiveday2/MyThread.java
@@ -1,15 +1,34 @@
 package learn.skillboxjavaintensive.intensiveday2;
 
+import com.dropbox.core.v2.DbxClientV2;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MyThread extends Thread {
+
+    public DbxClientV2 client;
+
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++){
-            System.out.println(i);
+        for (int i = 0; i < 5 ; i++){
             try {
-                sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyymmdd_hhmmss");
+                Date now = new Date();
+                String fileName = formatter.format(now) + ".png";
+                ByteArrayOutputStream os = new ByteArrayOutputStream();
+                ImageIO.write(image, "png", os);
+                client.files().uploadBuilder("/" + fileName).uploadAndFinish(new ByteArrayInputStream(os.toByteArray()));
+                System.out.println("Screenshot " + fileName + " was uploaded " + formatter.format(now));
+                sleep(5000);
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
     }
 }
+
